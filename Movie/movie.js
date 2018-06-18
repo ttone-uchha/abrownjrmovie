083a2bbbe779475d2a839d12d40ea59837ede569
Antonio <ttone.uchiha@gmail.com>
2018-06-18 11:12:49 -0500

    movie app

---
+++ Movie/movie.js
@@ -0,0 +1,14 @@
+alert('test');
+
+ //   var string = $("#inputforMovie").val();
+ //   var url = "http://www.omdbapi.com/?apikey=7ad73765&t=" + string;
+
+
+ // $("#movieSearch").click(function(){
+
+ //   $.getJSON(url, function(result){
+ //      //$("#movieBox").html("");
+ //      //$("#movieBox").append("");
+ //      console.log(result);
+ //   });
+
